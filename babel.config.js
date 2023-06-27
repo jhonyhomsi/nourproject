bbc6f35d54d1318be4cf7f02a2096786c2842d05
jhony <johnny.aj.antonio@gmail.com>
2023-06-27 14:39:39 +0300

    Update2 Of Babel File

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,8 @@
 module.exports = function(api) {
   api.cache(true);
+
   return {
     presets: ['babel-preset-expo'],
-    transformer: {
-      babelTransformerPath: require.resolve("react-native-svg-transformer"),
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: false,
-        },
-        babelOptions: {
-          plugins: ["@babel/plugin-transform-export-namespace-from"],
-        },
-      }),
-    },
+    plugins: ['@babel/plugin-transform-export-namespace-from'],
   };
 };
