c02ee8a20c70c2403dfbc21db1f483e61fe66f2f
jhony <johnny.aj.antonio@gmail.com>
2023-06-27 14:15:01 +0300

    ServerUpdate

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -8,7 +8,7 @@ const Login = ({ navigation }) => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('http://192.168.1.117:5000/login', {
+      const response = await fetch('https://nourapp.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
